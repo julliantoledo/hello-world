89ead8c097caa3a98b52bd80ba365e8962352cd5
julliantoledo <47582765+julliantoledo@users.noreply.github.com>
2019-02-13 23:56:19 -0800

    Add files via upload

---
+++ hello_world.cpp
@@ -0,0 +1,15 @@
+/*
+ * File: hello_world.cpp
+ * Author: Jullian Toledo
+ * Created on February 13th, 2019, 11:49 PM
+ * Purpose: First program Hello World
+ */
+
+#include <iostream> 
+using namespace std;
+
+int main() 
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
